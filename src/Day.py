8ff3e288969cc953ab66c4a25af83e8cc6c0685a
isaiah <isaiahsherfick8@gmail.com>
2021-05-09 23:05:15 -0400

    quick commit

--- src/Day.py
+++ src/Day.py
@@ -1,3 +1,14 @@
 
+class Day:
+    def __init__(self,day=None):
+        if day == None:
+            self.day = 1
+        else:
+            self.day = day
+        self.eventMap = {}
+        for i in range(24):
+            self.eventMap.update({i:None})
 
+    def getEventByHour(hour):
+        return 
 
