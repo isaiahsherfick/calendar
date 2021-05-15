85a9a8ab76e7f5d094c52c51e5008fd00d4eb879
isaiah <isaiahsherfick8@gmail.com>
2021-05-14 22:50:56 -0400

    finished year

--- src/Month.py
+++ src/Month.py
@@ -1,25 +1,12 @@
 from enum import Enum
 from Day import *
-class Months(Enum):
-    JANUARY = 1
-    FEBRUARY = 2
-    MARCH = 3
-    APRIL = 4
-    MAY = 5
-    JUNE = 6
-    JULY = 7
-    AUGUST = 8
-    SEPTEMBER = 9
-    OCTOBER = 10
-    NOVEMBER = 11
-    DECEMBER = 12
 class Month:
     def __init__(self,month=None,leapYear=False):
         self.month = month
         if not leapYear:
-            self.monthDict = {None:0,Months.JANUARY:31,Months.FEBRUARY:28,Months.MARCH:31,Months.APRIL:30,Months.MAY:31,Months.JUNE:30,Months.JULY:31,Months.AUGUST:31,Months.SEPTEMBER:30,Months.OCTOBER:31,Months.NOVEMBER:30,Months.DECEMBER:31}
+            self.monthDict = {None:0,1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
         else:
-            self.monthDict = {None:0,Months.JANUARY:31,Months.FEBRUARY:29,Months.MARCH:31,Months.APRIL:30,Months.MAY:31,Months.JUNE:30,Months.JULY:31,Months.AUGUST:31,Months.SEPTEMBER:30,Months.OCTOBER:31,Months.NOVEMBER:30,Months.DECEMBER:31}
+            self.monthDict = {None:0,1:31,2:29,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
         self.dayList = []
         if self.month in self.monthDict:
             for i in range(1,self.monthDict.get(self.month)+1):
@@ -37,17 +24,18 @@ class Month:
             print(d)
 
     def __repr__(self):
-        return f"{Month #self.month}"
+        return f"Month #{self.month}"
 
 
 #test main
 if __name__ == '__main__':
-    m = Month(Months.JANUARY)
+    #deprecated none of this works now
+    m = Month(1)
     print(m.getDays())
     m.displayDayList()
-    m=Month(Months.FEBRUARY,True)
+    m=Month(2,True)
     print(m.getDays())
     m.displayDayList()
-    m=Month(Months.FEBRUARY)
+    m=Month(2)
     print(m.getDays())
     m.displayDayList()
--- src/Year.py
+++ src/Year.py
@@ -1,5 +1,14 @@
 from Month import *
 
+def isLeapYear(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    return False
+
 class Year:
     def __init__(self,year=None):
         if year == None:
@@ -8,4 +17,27 @@ class Year:
             self.year = year
         self.months = {}
         for i in range(1,13):
-            self.months.update({i:Month(i)})
+            if isLeapYear(self.year):
+                self.months.update({i:Month(i,True)})
+            else:
+                self.months.update({i:Month(i,False)})
+                pass
+        #for i in self.months:
+            #print(f"{i} : {self.months.get(i)}")
+
+    def getMonth(self,month):
+        return self.months.get(month)
+
+    def isLeapYear(self):
+        return isLeapYear(self.year)
+
+
+
+if __name__ == '__main__':
+    #for i in range(2000,2022):
+        #print(f"{i} is a leap year: {isLeapYear(i)}")
+
+    y = Year(2020)
+    print(y.getMonth(4))
+    print(y.getMonth(5).getDay(17))
+
